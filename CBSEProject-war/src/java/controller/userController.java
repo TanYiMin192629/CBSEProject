e4a5635d88d8ae6d451d8f567892625b34464eaf
TanYiMin192629 <yimin_0430@hotmail.com>
2020-07-17 02:58:29 +0800

    Manage Event

--- CBSEProject-war/src/java/controller/userController.java
+++ CBSEProject-war/src/java/controller/userController.java
@@ -2,11 +2,13 @@
 package controller;
 
 import ejb.Login;
+import ejb.Profile;
 import entities.Users;
 import model.UsersFacade;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.persistence.Persistence;
 
@@ -19,6 +21,9 @@ import javax.persistence.Persistence;
 public class userController implements Serializable {
 
     @EJB
+    private Profile profile;
+
+    @EJB
     private Login login1;
 
     @EJB
@@ -26,6 +31,8 @@ public class userController implements Serializable {
 
     
     private Users user = new Users();
+    
+    
 
     public Users getUser() {
         return user;
@@ -33,6 +40,18 @@ public class userController implements Serializable {
 
     public void setUser(Users user) {
         this.user = user;
+    }
+    
+    
+    public List<Users> findAll(){
+        return this.usersFacade.findAll();
+    
+    }
+    
+    public List<Users> findUser(){
+        
+        return this.profile.findUsers(this.user.getEmail(),this.user.getPassword());
+ 
     }
 
     public String register() {
@@ -47,9 +66,15 @@ public class userController implements Serializable {
         System.out.print(this.user.getEmail());
         System.out.print(this.user.getPassword());
         if( login1.authenticate(this.user.getEmail(),this.user.getPassword())){
+            this.user = user;
             return "user_homepage";
         }
         return "index";
+    }
+    
+    public void edit(Users user){
+        this.usersFacade.edit(this.user);
+    
     }
 
     public userController() {
